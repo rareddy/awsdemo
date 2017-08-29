10a424c934ed73841dae86b73388323f2123e722
Ramesh Reddy <rareddy@jboss.org>
2017-08-29 13:19:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,106 +10,25 @@ import os,json
 def index():
     return static_file("index.html", root='./')
 
-
-
-@get('/ws/zips')
+@get('/ws/animals')
 def getzips():
     results = []
     try:
-        conn = psycopg2.connect(database=os.environ.get('POSTGRES_DB'), user=os.environ.get('POSTGRES_USER'),
-                                host=os.environ.get('POSTGRES_HOST'), password=os.environ.get('POSTGRES_PASSWORD'))
+        conn = psycopg2.connect(database=os.environ.get('DB_NAME'), user=os.environ.get('DB_USER'),
+                                host=os.environ.get('DB_HOST'), password=os.environ.get('DB_PASSWORD'))
     except:
-        print("couldn't make connection" + os.environ.get('POSTGRES_HOST'))
+        print("couldn't make connection" + os.environ.get('DB_HOST'))
 
     cur = conn.cursor()
-    cur.execute("""select zipcode, count, ST_AsText(the_geom) from zipcodes""")
+    cur.execute("""select id, name, type from animal""")
     rows = cur.fetchall()
 
     for row in rows:
         result = {}
-        result = {'zipcode': row[0], 'count': row[1]}
-        coords = []
-        temp_coords = row[2]
-        lon = temp_coords[temp_coords.find('(')+ 1:temp_coords.find(' ')]
-        lat = temp_coords[temp_coords.find(' '):temp_coords.find(')')]
-        coords = [lon, lat]
-        result['coords'] = coords
+        result = {'id': row[0], 'name': row[1], 'type': row[2]}
         results.append(result)
 
     return json.dumps({'results': list(results)})
-
-@get('/ws/airports')
-def getairports():
-    results = []
-    try:
-        conn = psycopg2.connect(database=os.environ.get('POSTGRES_DB'), user=os.environ.get('POSTGRES_USER'),
-                                host=os.environ.get('POSTGRES_HOST'), password=os.environ.get('POSTGRES_PASSWORD'))
-    except:
-        print(os.environ.get('POSTGRES_HOST'))
-
-    cur = conn.cursor()
-    cur.execute("""select name, passengers, ST_AsText(the_geom) from airports""")
-    rows = cur.fetchall()
-
-    for row in rows:
-        result = {}
-        result = {'name': row[0], 'passengers': row[1]}
-        coords = []
-        temp_coords = row[2]
-        lon = temp_coords[temp_coords.find('(')+ 1:temp_coords.find(' ')]
-        lat = temp_coords[temp_coords.find(' '):temp_coords.find(')')]
-        coords = [lon, lat]
-        result['coords'] = coords
-        results.append(result)
-
-    return json.dumps({'results': list(results)})
-
-
-
-
-#This is for the parkpoints data set which may or may not be there
-@get('/db')
-def dbexample():
-    try:
-        conn = psycopg2.connect(database=os.environ.get('POSTGRES_DB'), user=os.environ.get('POSTGRES_USER'),
-                            host=os.environ.get('POSTGRES_HOST'), password=os.environ.get('POSTGRES_PASSWORD'))
-    except:
-        print(os.environ.get('POSTGRES_HOST'))
-
-    cur = conn.cursor()
-    # cur.execute("""select parkid, name, ST_AsText(the_geom) from parkpoints limit 10""")
-    cur.execute("""select parkid, name, ST_AsText(the_geom) from parkpoints ORDER by parkid DESC LIMIT 10""")
-
-    rows = cur.fetchall()
-    result_string = "<h2>Here are your results: </h2>"
-    for row in rows:
-        result_string += "<h3>" + str(row[0]) + ", " + str(row[1]) + ", " + str(row[2]) + "</h3>"
-
-    cur.close()
-    conn.close()
-
-    return result_string
-
-
-
-
-#For Static files
-
-@get("/static/css/<filename:re:.*\.css>")
-def css(filename):
-    return static_file(filename, root="static/css")
-
-@get("/static/font/<filename:re:.*\.(eot|otf|svg|ttf|woff|woff2?)>")
-def font(filename):
-    return static_file(filename, root="static/font")
-
-@get("/static/img/<filename:re:.*\.(jpg|png|gif|ico|svg)>")
-def img(filename):
-    return static_file(filename, root="static/img")
-
-@get("/static/js/<filename:re:.*\.js>")
-def js(filename):
-    return static_file(filename, root="static/js")
 
 
 if __name__ == '__main__':
